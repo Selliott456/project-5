7ab4755946be279476a967fe14c95ce94a97593b
Sherryll Elliott <sherryllelliott@gmail.com>
2020-12-14 20:02:11 +0000

    user models and evironment config added

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,23 @@
 from flask import Flask
+from environment.config import db_URI
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+from flask_bcrypt import Bcrypt
+
 app = Flask(__name__)
 
-# ! Hello world flask app to start you off.
-@app.route('/')
-def index():
-    return "Hello, World!"
+app.config['SQLALCHEMY_DATABASE_URI'] = db_URI
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+ma = Marshmallow(app)
+
+bcrypt = Bcrypt(app)
+
+from controllers import pictures, users
+
+app.register_blueprint(pictures.router, url_prefix="/api")
+app.register_blueprint(users.router, url_prefix="/api")
+
+
+
---
+++ backend/environment/config.py
@@ -0,0 +1,3 @@
+db_URI = 'postgres://localhost:5432/teas_db'
+# ! Moved my secret in here
+secret = 'This is our secret string blah blah blah'
